a1817c99d3bdfa537459e65b1771193fc2802043
zjjsven <dlzhjj@cn.ibm.com>
2016-06-27 17:27:45 +0800

    test 3

--- routes/admin/books.js
+++ routes/admin/books.js
@@ -71,7 +71,7 @@ module.exports = function(app) {
         res.json({
           'errType': 0
         });
-        Mail.sendEmail('dlzhjj@cn.ibm.com', '[Elevenlibrary]Admin modified the book :' + mdfBook.name, 'Welcome to ElevenLibrary. \nYour book ' + mdfBook.name + ' has been deleted by adminstrator. \nURL: http://localhost/project/elevenlibrary-frontend/#/manage/books .');
+        Mail.sendEmail('dlzhjj@cn.ibm.com', '[Elevenlibrary]Admin modified the book :' + mdfBook.name, 'Your book ' + mdfBook.name + ' has been deleted by adminstrator. ', 'http://localhost/project/elevenlibrary-frontend/#/manage/books');
       }
     });
   });
