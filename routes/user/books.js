d75fc5911d4bf8819c115dc8382a6cf71b5efb5f
zjjsven <dlzhjj@cn.ibm.com>
2016-06-28 02:04:35 +0800

    test mail 6

--- routes/user/books.js
+++ routes/user/books.js
@@ -113,7 +113,7 @@ module.exports = function(app) {
           errType: 0,
           _id: newBook._id
         });
-        Mail.sendEmail(Mail.admin, '[Elevenlibrary]Book '+newBook.name+' has been uploaded by '+newBook.ownerIntrID, 'Book '+newBook.name+' has been uploaded by '+ newBook.ownerIntrID+', please confirm and approve the request.', 'book/'+book._id);
+        Mail.sendEmail(Mail.admin, '[Elevenlibrary]Book '+newBook.name+' has been uploaded by '+newBook.ownerIntrID, 'Book '+newBook.name+' has been uploaded by '+ newBook.ownerIntrID+', please confirm and approve the request.', 'book/'+newBook._id);
       }
     });
   });
@@ -133,7 +133,7 @@ module.exports = function(app) {
         res.json({
           'errType': 0
         });
-        Mail.sendEmail(Mail.admin, '[Elevenlibrary]The information of the book '+newBook.name+' has been updated by '+ newBook.ownerIntrID, 'The information of the book '+newBook.name+' has been updated by '+ newBook.ownerIntrID +', please confirm and approve the request.','book/'+book._id);
+        Mail.sendEmail(Mail.admin, '[Elevenlibrary]The information of the book '+newBook.name+' has been updated by '+ newBook.ownerIntrID, 'The information of the book '+newBook.name+' has been updated by '+ newBook.ownerIntrID +', please confirm and approve the request.','book/'+_id);
       }
     });
   });
